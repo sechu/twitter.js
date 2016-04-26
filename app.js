fa024df2ce58d9c90b59d22200e8c20e4fe72525
sechu <sabrina.chu@gmail.com>
2016-04-26 16:02:18 -0400

    milestone 1

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+
+app.use(function(request, response, next) {
+	console.log(request.method, request.path, response.statusCode);
+	next();
+});
+
+app.use('/special', function(request, response, next) {
+	console.log("special logger");
+	next();
+});
+
+app.get('/', function(request, response) {
+	response.send('hello world');
+});
+
+
+var server = app.listen(3000, function() {
+	console.log('server started');
+});
+
